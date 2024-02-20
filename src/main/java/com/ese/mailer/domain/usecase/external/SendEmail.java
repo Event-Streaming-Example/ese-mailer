ad141cb8d9a86ab94ab95f6dbcbfeb89a89b8194
Saumya Bhatt <saumya.bhatt106@gmail.com>
2024-02-21 03:08:27 +0530

    Code review

--- src/main/java/com/ese/mailer/domain/usecase/external/SendEmail.java
+++ src/main/java/com/ese/mailer/domain/usecase/external/SendEmail.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 @Service
